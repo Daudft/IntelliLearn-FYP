af4dfe09035e901e5387df1b98bff9b557147d8d
Daudft <alikhaft45@gmail.com>
2025-11-29 15:21:41 +0500

    30% completed

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,12 @@ import SignIn from "./pages/Auth/SignIn";
 import SignUp from "./pages/Auth/SignUp";
 import ForgotPassword from "./pages/Auth/ForgotPassword";
 import VerifyEmail from "./pages/Auth/VerifyEmail";
-import ResetPassword from "./pages/Auth/ResetPassword"; // <-- ADD THIS
+import ResetPassword from "./pages/Auth/ResetPassword";
+
+// Assessment Pages - ✅ NEW IMPORTS
+import LanguageSelection from "./pages/Assessment/LanguageSelection";
+import AssessmentTest from "./pages/Assessment/AssessmentTest";
+import AssessmentResult from "./pages/Assessment/AssessmentResult";
 
 function App() {
   return (
@@ -26,8 +31,13 @@ function App() {
         {/* Must include token param */}
         <Route path="/verify-email/:token" element={<VerifyEmail />} />
 
-        {/* New Reset Password Route */}
+        {/* Reset Password Route */}
         <Route path="/reset-password/:token" element={<ResetPassword />} />
+
+        {/* Assessment Routes - ✅ NEW ROUTES */}
+        <Route path="/assessment" element={<LanguageSelection />} />
+        <Route path="/assessment/test/:language" element={<AssessmentTest />} />
+        <Route path="/assessment/result" element={<AssessmentResult />} />
 
       </Routes>
     </BrowserRouter>
